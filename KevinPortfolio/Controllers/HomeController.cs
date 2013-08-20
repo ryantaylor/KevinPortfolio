6ee4143f8fd20496d9708eb281bf2a5a61c25dc4
ryantaylor <ryan.taylor9@gmail.com>
2013-08-20 12:21:26 -0400

    Added Bootstrap.

--- KevinPortfolio/Controllers/HomeController.cs
+++ KevinPortfolio/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace KevinPortfolio.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BootstrapBaseController
     {
         public ActionResult Index()
         {
